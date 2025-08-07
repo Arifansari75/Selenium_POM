a59e0974f3bc47b9b53a2c3ee6e2f55526b749db
Arifansari75 <arifansari1899@gmail.com>
2025-08-07 12:26:28 +0530

    added test2

--- Tests/test_google_home_page.py
+++ Tests/test_google_home_page.py
@@ -12,13 +12,17 @@ class GoogleHomeTest(slash.Test):
     def before(self,Driver):
         print("Inside before")
         self.googleHomePage = GoogleHomePage(Driver)
+        self.googleHomePage.load_url()
         print("outside before")
 
     def test_test1(self):
-        self.googleHomePage.load_url()
         list_of_google_apps = self.googleHomePage.list_of_all_google_apps()
         print(len(list_of_google_apps))
         print("Inside test1",list_of_google_apps)
         # self.googleHomePage.google_search_with_text("Python W3 school")
         list_of_language = self.googleHomePage.list_of_all_languages()
         print(len(list_of_language),list_of_language)
+
+    def test_test2(self):
+        self.googleHomePage.google_search_with_text("Python W3 School")
+
